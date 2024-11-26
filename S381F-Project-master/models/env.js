76730257e2321aa2a17d5600e969ddd2bd7957b4
ElvisChanHK <144596490+ElvisChanHK@users.noreply.github.com>
2024-11-26 08:54:04 +0800

    Update env.js

--- S381F-Project-master/models/env.js
+++ S381F-Project-master/models/env.js
@@ -1,4 +1,4 @@
-const MONGO_CONNECTION_URL='';
+const MONGO_CONNECTION_URL='mongodb+srv://s1334520:<Elvis689>@cluster0.nyublhb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 const SESSION_SECRET = '';
 const COOKIE_MAXAGE = 30 * 60; // 30 minutes
 
